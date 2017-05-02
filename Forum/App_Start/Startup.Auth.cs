40d73a7a716f6e443b0f07c0c9952b990034885a
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-02 09:41:49 -0400

    twitter outh working

--- Forum/App_Start/Startup.Auth.cs
+++ Forum/App_Start/Startup.Auth.cs
@@ -6,6 +6,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Forum.Models;
+using Microsoft.Owin.Security.Twitter;
+using Microsoft.Owin.Security;
 
 namespace Forum
 {
@@ -50,9 +52,24 @@ namespace Forum
             //    clientId: "",
             //    clientSecret: "");
 
-            app.UseTwitterAuthentication(
-               consumerKey: "tBMdKWYfYtC9YUIkjUiSTvXMD",
-               consumerSecret: "	v6F7e3xAElx8eEeCGyN8hcek8Y1i2IEEWhX5kDVKjl3rnoLBdS");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = "tBMdKWYfYtC9YUIkjUiSTvXMD",
+                ConsumerSecret = "v6F7e3xAElx8eEeCGyN8hcek8Y1i2IEEWhX5kDVKjl3rnoLBdS",
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+    {
+        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+        "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
+        "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+    })
+            });
+
+            //app.UseTwitterAuthentication(
+               //consumerKey: "tBMdKWYfYtC9YUIkjUiSTvXMD",
+               //consumerSecret: "	v6F7e3xAElx8eEeCGyN8hcek8Y1i2IEEWhX5kDVKjl3rnoLBdS");
 
             //app.UseFacebookAuthentication(
             //   appId: "",
